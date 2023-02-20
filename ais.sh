0dcc00ddbea6588c78c6771db678c2efced5051d
notkumane <93247376+notkumane@users.noreply.github.com>
2023-02-20 12:20:38 +0200

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -1,84 +1,65 @@
 #!/bin/bash
 
-# Prompt for the root password
-read -sp 'Enter the root password: ' ROOT_PASS
-echo ""
+echo "Setting up the disks..."
 
-# Prompt for the hostname
-read -p 'Enter the hostname: ' HOST_NAME
+lsblk
 
-# Partition the disk
-cfdisk /dev/sda
+echo -n "Enter the device to use as root (eg. /dev/sda): "
+read ROOT_DEVICE
 
-# Format the partitions
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
-mkfs.ext4 /dev/sda4
+echo -n "Enter the swap size in GiB (eg. 2): "
+read SWAP_SIZE
 
-# Mount the partitions
-swapon /dev/sda2
-mount /dev/sda3 /mnt
+echo "Setting up the partitions..."
+
+sgdisk --zap-all $ROOT_DEVICE
+sgdisk --new=1:0:+512MiB --typecode=1:ef00 $ROOT_DEVICE
+sgdisk --new=2:0:+${SWAP_SIZE}GiB --typecode=2:8200 $ROOT_DEVICE
+sgdisk --new=3:0:0 --typecode=3:8300 $ROOT_DEVICE
+mkfs.fat -F32 ${ROOT_DEVICE}1
+mkswap ${ROOT_DEVICE}2
+mkfs.ext4 ${ROOT_DEVICE}3
+swapon ${ROOT_DEVICE}2
+mount ${ROOT_DEVICE}3 /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir /mnt/home
-mount /dev/sda4 /mnt/home
+mount ${ROOT_DEVICE}1 /mnt/boot
 
-# Install the base system
-pacstrap /mnt base base-devel intel-ucode linux-zen linux-zen-headers dkms networkmanager neovim
+echo "Installing the base system..."
 
-# Generate fstab
+pacstrap /mnt base linux linux-firmware neovim networkmanager
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Chroot into the new system
+echo "Chrooting into the new system..."
+
+echo "Enter the hostname:"
+read HOSTNAME
+
 arch-chroot /mnt /bin/bash <<EOF
-
-# Set the root password
-echo "root:$ROOT_PASS" | chpasswd
-
-# Uncomment en_US.UTF-8 in /etc/locale.gen and generate locale
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-
-# Set the LANG variable in /etc/locale.conf
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Set the hostname
-echo "$HOST_NAME" > /etc/hostname
-
-# Update /etc/hosts file
-echo "127.0.0.1    localhost" >> /etc/hosts
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1    localhost" > /etc/hosts
 echo "::1          localhost" >> /etc/hosts
-echo "127.0.1.1    $HOST_NAME.localdomain    $HOST_NAME" >> /etc/hosts
+echo "127.0.1.1    $HOSTNAME.localdomain    $HOSTNAME" >> /etc/hosts
 
-# Enable NetworkManager service
-systemctl enable NetworkManager
+echo "Enter the root password:"
+passwd
 
-# Install and configure grub
-pacman -S grub efibootmgr dosfstools os-prober mtools --noconfirm
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
-grub-mkconfig -o /boot/grub/grub.cfg
+echo "Enter the notkeemane user password:"
+useradd -m -G wheel -s /bin/bash notkeemane
+passwd notkeemane
 
-# Create user
-useradd -m -G wheel,audio,video,optical,storage notkeemane
+sed -i 's/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/' /etc/sudoers
+ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
+hwclock --systohc
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+systemctl enable NetworkManager.service
 
-# Set user password
-read -sp 'Enter password for user notkeemane: ' USER_PASS
-echo ""
-echo "notkeemane:$USER_PASS" | chpasswd
+echo "Installation complete. Reboot now? (y/n)"
+read REBOOT
 
-# Allow user to use sudo without password
-echo "notkeemane ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-EOF
-
-# Ask the user whether to reboot or stay in chroot
-read -p "Installation complete. Reboot now? (y/n) " REBOOT
-
-# If user enters "y", reboot the system
-if [[ "$REBOOT" =~ [Yy] ]]; then
-    umount -R /mnt
-    reboot
+if [ "$REBOOT" == "y" ]; then
+  reboot
+else
+  echo "Exiting chroot. You may now unmount the partitions and reboot."
 fi
+EOF
