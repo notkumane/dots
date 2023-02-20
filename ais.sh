83c436973aa086442cb19c50a9524a291a7ac609
notkumane <93247376+notkumane@users.noreply.github.com>
2023-02-20 11:53:34 +0200

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Prompt for the root password
-echo "Enter the root password:"
-read -s ROOT_PASS
+read -sp 'Enter the root password: ' ROOT_PASS
+echo ""
+
+# Prompt for the hostname
+read -p 'Enter the hostname: ' HOST_NAME
 
 # Partition the disk
 cfdisk /dev/sda
@@ -22,7 +25,7 @@ mkdir /mnt/home
 mount /dev/sda4 /mnt/home
 
 # Install the base system
-pacstrap /mnt base base-devel intel-ucode linux-zen linux-zen-headers dkms neovim networkmanager
+pacstrap /mnt base base-devel intel-ucode linux-zen linux-zen-headers dkms networkmanager neovim
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -33,17 +36,23 @@ arch-chroot /mnt /bin/bash <<EOF
 # Set the root password
 echo "root:$ROOT_PASS" | chpasswd
 
-# Enable parallel downloads in pacman.conf
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
-
-# Uncomment en_US.UTF-8 UTF-8 in /etc/locale.gen
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-
-# Generate the locale
+# Uncomment en_US.UTF-8 in /etc/locale.gen and generate locale
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
 # Set the LANG variable in /etc/locale.conf
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Set the hostname
+echo "$HOST_NAME" > /etc/hostname
+
+# Update /etc/hosts file
+echo "127.0.0.1    localhost" >> /etc/hosts
+echo "::1          localhost" >> /etc/hosts
+echo "127.0.1.1    $HOST_NAME.localdomain    $HOST_NAME" >> /etc/hosts
+
+# Enable NetworkManager service
+systemctl enable NetworkManager
 
 # Install and configure grub
 pacman -S grub efibootmgr dosfstools os-prober mtools --noconfirm
@@ -52,36 +61,24 @@ mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Set the hostname
-echo "Enter the hostname:"
-read HOSTNAME
-echo "$HOSTNAME" > /etc/hostname
-
-# Set up hosts file
-echo "127.0.0.1	localhost" >> /etc/hosts
-echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1	$HOSTNAME.localdomain	$HOSTNAME" >> /etc/hosts
-
 # Create user
 useradd -m -G wheel,audio,video,optical,storage notkeemane
 
 # Set user password
-echo "Enter password for user notkeemane:"
-read -s USER_PASS
+read -sp 'Enter password for user notkeemane: ' USER_PASS
+echo ""
 echo "notkeemane:$USER_PASS" | chpasswd
 
 # Allow user to use sudo without password
 echo "notkeemane ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-# Enable NetworkManager service
-systemctl enable NetworkManager.service
-
 EOF
 
-# Prompt for reboot
-read -p "Installation complete. Reboot now? [y/n]: " REBOOT
-if [ "$REBOOT" = "y" ]; then
-  reboot
-else
-  echo "You can now exit chroot and reboot later to boot into the new system."
+# Ask the user whether to reboot or stay in chroot
+read -p "Installation complete. Reboot now? (y/n) " REBOOT
+
+# If user enters "y", reboot the system
+if [[ "$REBOOT" =~ [Yy] ]]; then
+    umount -R /mnt
+    reboot
 fi
