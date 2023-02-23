9851f3b5aea009614f845aa8cd11c026613aa248
notkumane <93247376+notkumane@users.noreply.github.com>
2023-02-23 14:24:39 +0200

    updates

--- ais.sh
+++ ais.sh
@@ -1,70 +1,49 @@
 #!/bin/bash
 
-# List available drives
+#!/bin/bash
 printf "Available drives:\n"
-drives=( $(lsblk -dpn | grep -Ev "boot|rpmb|loop" | awk '{print $1}') )
-
-# Check if any drives are available
+drives=($(lsblk -dpn | grep -Ev "boot|rpmb|loop" | awk '{print $1}'))
 if [ ${#drives[@]} -eq 0 ]; then
-    printf "No drives found.\n"
-    exit 1
+printf "No drives found.\n"
+exit 1
 fi
-
-# Display the list of available drives
 for i in "${!drives[@]}"; do
-    printf "%d. %s\n" "$i" "${drives[$i]}"
+printf "%d.%s\n" "$i" "${drives[$i]}"
 done
-
-# Prompt the user to select a drive for partitioning
 read -rp "Enter the number of the drive to partition: " drive_num
-
-# Verify that the selected drive number is valid
 if ! [[ "$drive_num" =~ ^[0-9]+$ ]] || [ "$drive_num" -lt 0 ] || [ "$drive_num" -ge ${#drives[@]} ]; then
-    printf "Invalid drive number.\n"
-    exit 1
+printf "Invalid drive number.\n"
+exit 1
 fi
-
-# Get the name of the selected drive
 drive="${drives[$drive_num]}"
-
-# Confirm the selected drive with the user
 read -rp "You have selected $drive for partitioning. Are you sure? (y/n): " confirm
-
 if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]; then
-    printf "Partitioning cancelled.\n"
-    exit 1
+printf "Partitioning cancelled.\n"
+exit 1
 fi
-
-# Verify that the selected drive exists
 if ! [ -b "$drive" ]; then
-    printf "Drive %s does not exist.\n" "$drive"
-    exit 1
+printf "Drive %s does not exist.\n" "$drive"
+exit 1
 fi
-
-# Wipe the drive with wipefs
-printf "Wiping drive %s...\n" "$drive"
 wipefs -a "$drive"
-
-# Partition the drive with gdisk
-printf "Partitioning drive %s...\n" "$drive"
-printf "Creating partition 1 (EFI System Partition)...\n"
+sgdisk -g "$drive"
 sgdisk -n 1:0:+512MiB -t 1:EF00 "$drive"
-printf "Creating partition 2 (Linux Swap)...\n"
 sgdisk -n 2:0:+8GiB -t 2:8200 "$drive"
-printf "Creating partition 3 (Linux Filesystem)...\n"
 sgdisk -n 3:0:0 -t 3:8300 "$drive"
-
-# Format the partitions
-printf "Formatting partitions...\n"
 mkfs.fat -F32 "${drive}1"
 mkswap "${drive}2"
 swapon "${drive}2"
-mkfs.ext4 "${drive}3"
-
-# Mount the partitions
-printf "Mounting partitions...\n"
+mkfs.btrfs -f "${drive}3"
 mount --mkdir "${drive}3" /mnt
-mount --mkdir "${drive}1" /mnt/boot
+btrfs subvolume create /mnt/@root
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@snapshots
+umount /mnt
+mount -o subvol=@root "${drive}3" /mnt
+mkdir /mnt/{home,var,.snapshots}
+mount -o subvol=@home "${drive}3" /mnt/home
+mount -o subvol=@var "${drive}3" /mnt/var
 
 # Prompts for root password, notkeemane password and hostname
 echo "Enter password for root user:"
@@ -91,7 +70,7 @@ echo "Server = https://mirrors.lavatech.top/archlinux/\$repo/os/\$arch" >> /etc/
 # Enable parallel downloads
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 
-pacstrap /mnt base base-devel linux-zen linux-zen-headers intel-ucode networkmanager
+pacstrap /mnt base base-devel linux-zen linux-zen-headers dkms intel-ucode networkmanager linux-firmware
 
 # Generating the fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -138,8 +117,7 @@ cd /tmp
 git clone https://github.com/notkumane/dots
 cd dots
 cp .xinitrc .xprofile .zshenv /home/notkeemane
-mkdir -p /home/notkeemane/.zsh
-cp -r .zsh /home/notkeemane/.zsh
+cp -r .zsh /home/notkeemane
 mkdir -p /home/notkeemane/.config/i3
 cp config /home/notkeemane/.config/i3
 cp starship.toml picom.conf /home/notkeemane/.config
@@ -148,15 +126,25 @@ cp starship.toml picom.conf /home/notkeemane/.config
 cd /home/notkeemane/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# Install packages with yay
-yay -S --needed --noconfirm wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+# Enable multilib and sync databases
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo pacman -Sy
+
+# Install Wine dependencies
+yay -S --needed --noconfirm giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
 mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
 lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
 sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
 ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lutris steam
 
-yay -S --noconfirm nvidia xorg-server xorg-xinit xorg-xset xorg-xrandr i3-gaps nitrogen xfce4-panel xfce4-notifyd autotiling brave-bin gnome-screenshot xfce4-terminal thunar xarchiver gvfs unrar picom xdg-utils xdg-user-dirs ristretto lxappearance zsh neovim exa htop xfce4-i3-workspaces-plugin-git starship gamemode
+# Install Nvidia drivers and other Xorg-related packages
+yay -S --needed --noconfirm nvidia xorg-server xorg-xinit xorg-xset xorg-xrandr
+
+# Install i3-gaps and other window manager-related packages
+yay -S --needed --noconfirm i3-gaps nitrogen xfce4-panel xfce4-notifyd autotiling \
+brave-bin gnome-screenshot xfce4-terminal thunar xarchiver gvfs unrar picom xdg-utils \
+xdg-user-dirs ristretto lxappearance zsh neovim exa htop xfce4-i3-workspaces-plugin-git starship gamemode ttf-firacode-nerd
 
 # Install and configure bootloader
 pacman -S --noconfirm grub efibootmgr
