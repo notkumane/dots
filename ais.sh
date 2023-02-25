4867d4f6083429a8bfac12b42827755647632a26
notkumane <93247376+notkumane@users.noreply.github.com>
2023-02-25 23:30:45 +0200

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -189,8 +189,10 @@ mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32
 lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
 sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
 ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
-lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-yay -S --noconfirm nvidia xorg-server xorg-xinit xorg-xset xorg-xrandr plasma-desktop kscreen sddm dolphin konsole kdeplasma-addons kde-gtk-config brave kate gamemode steam lutris
+lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader \
+zsh starship exa gvfs xdg-utils xdg-user-dirs nvidia xorg-server xorg-xinit xorg-xset \
+xorg-xrandr plasma-desktop kscreen sddm dolphin konsole kdeplasma-addons kde-gtk-config kate gamemode steam lutris
+
 systemctl enable sddm 
 
 # Install bootloader
